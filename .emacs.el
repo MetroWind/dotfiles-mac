2b79fbfac406e4b5263b5cc5552135dfa0dbc08d
M. Sun <chris.corsair@gmail.com>
2012-11-24 20:31:11 -0500

    Emacs: Read PATH from a shell.
    On Mac, applications are usually not started by a shell.  Therefore
    Emacs may not have the full PATH variable.

--- .emacs.el
+++ .emacs.el
@@ -249,7 +249,20 @@
 ;; Mac.
 (if macp (setq ns-pop-up-frames nil))
 ;; PATH
-(setq exec-path (cons "/usr/local/bin" exec-path))
+;; (setq exec-path (cons "/usr/local/bin" exec-path))
+(defun set-exec-path-from-shell-PATH ()
+  "Set up `exec-path' and PATH environment variable to match that
+used by the user's shell.  This is particularly useful in Mac,
+where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string 
+                          "[ \t\n]*$"
+                          ""
+                          (shell-command-to-string
+                           "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(if macp (set-exec-path-from-shell-PATH))
 
 ;; =============== C Mode ===============>
 (add-hook 'c-mode-common-hook

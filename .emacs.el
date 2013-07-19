7e03c01ec3ff3159059cc1360dc466536c792fa0
M. Sun <chris.corsair@gmail.com>
2013-07-19 13:23:41 -0400

    Emacs: Make modeline shorter by use short minor mode strings.

--- .emacs.el
+++ .emacs.el
@@ -1115,6 +1115,38 @@ followed by a dash to an em-dash."
    (linuxp (setq fancy-splash-image my-splash-linux))
    (macp (setq fancy-splash-image my-splash-mac))))
 
+;; Shorter modeline
+(defvar mode-line-cleaner-alist
+  `((auto-complete-mode . " α")
+    (yas/minor-mode . " υ")
+    (git-gutter-mode . " γ")
+    (company-mode . " κ")
+    (abbrev-mode . "")
+    ;; Major modes
+    (lisp-interaction-mode . "λ")
+    (hi-lock-mode . "")
+    (python-mode . "π")
+    (emacs-lisp-mode . "Λ")
+    )
+  "Alist for `clean-mode-line'.
+
+When you add a new element to the alist, keep in mind that you
+must pass the correct minor/major mode symbol and a string you
+want to use in the modeline *in lieu of* the original.")
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for cleaner in mode-line-cleaner-alist
+        do (let* ((mode (car cleaner))
+                 (mode-str (cdr cleaner))
+                 (old-mode-str (cdr (assq mode minor-mode-alist))))
+             (when old-mode-str
+                 (setcar old-mode-str mode-str))
+               ;; major mode
+             (when (eq mode major-mode)
+               (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
 (if macp
     (progn
       ;; (add-to-list 'default-frame-alist '(height . 60))

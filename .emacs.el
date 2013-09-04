0da497b8b630acf5bfc7063710cac2fb5f2641df
M. Sun <chris.corsair@gmail.com>
2013-09-04 10:49:49 -0400

    Emacs: Use python.el instead of python-mode.el.
    Python.el comes with Emacs.  Python-mode.el is from the PSF.
    Python.el is for Emacs 23+.

--- .emacs.el
+++ .emacs.el
@@ -314,54 +314,9 @@ where GUI apps are not started from a shell."
 (add-hook 'python-mode-hook
           (lambda () (subword-mode 1)))
 
-;; PSF Python mode
-(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist (cons '("python" . python-mode)
-                                   interpreter-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
-
-;; Python + Flymake
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
-
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-
-;; (add-hook 'python-mode-hook 'flymake-mode)
-
-;; Python + ElDoc
+;; Python mode (python.el)
 (add-hook 'python-mode-hook
-          (lambda () (eldoc-mode 1)) t)
-(defun my-python-documentation (w)
-  "Launch PyDOC on the Word at Point"
-  (interactive
-   (list (let* ((word (thing-at-point 'word))
-                (input (read-string
-                        (format "pydoc entry%s: "
-                                (if (not word) ""
-                                  (format " (default %s)" word))))))
-           (if (string= input "")
-               (if (not word) (error "No pydoc args given")
-                 word) ;sinon word
-             input)))) ;sinon input
-  (shell-command (concat py-python-command
-                         " -c \"from pydoc import help;help(\'" w "\')\"")
-                 "*PYDOCS*")
-  (view-buffer-other-window "*PYDOCS*" t 'kill-buffer-and-window))
-
-;; iPython
-(if linuxp
-    ((lambda ()
-      (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-      (require 'ipython)
-      (setq py-python-command-args '( "-colors" "Linux"))
-      (setq ipython-command "/usr/bin/ipython"))))
+          (lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
 
 ;; Whitespace mode
 (autoload 'whitespace-mode "whitespace" "Load whitespace" t)

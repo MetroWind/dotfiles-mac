bf4328f514c1444fbc28214c68c7923a332bc9c7
M. Sun <chris.corsair@gmail.com>
2014-07-21 13:59:25 -0400

    Emacs: Adjust org-mode.
    - Show whole content of a matching sparse tree.  This is mainly for
    LaTeX exporting convenience.
    
    - Properly hide leading stars
    
    - Use XeLaTeX for LaTeX PDF exporting.  Adjust macro packages accordingly.

--- .emacs-org.el
+++ .emacs-org.el
@@ -1,5 +1,9 @@
 ;; (require 'org-install)
 
+(message "Loading org configurations...")
+(require 'ox-latex)
+(require 'org-table)
+
 ;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
@@ -11,6 +15,10 @@
 (setq org-startup-folded nil)           ;Show all content on startup.
 (setq org-todo-keywords '((sequence "TODO" "|" "DONE" "CANCELED")))
 (setq org-agenda-dim-blocked-tasks t)
+
+;; When creating a sparse tree from matching, show the whole content
+;; of the sparse tree.  This is for convenient LaTeX export.
+(setq org-show-entry-below '((default . nil) (tags-tree . t) (occur-tree . t)))
 
 ;; Set to the location of your Org files on your local system
 (setq org-directory
@@ -37,14 +45,18 @@
           (lambda () (define-key org-mode-map (kbd "C-c C-a") 'show-all)))
 
 (setq org-hide-leading-stars t)
-(if (window-system)
-    (add-hook 'org-mode-hook
-              (lambda ()
-                (set-face-attribute
-                 'org-hide nil
-                 :foreground "#2e3735"))))
+;; (if (window-system)
+;;     (add-hook 'org-mode-hook
+;;               (lambda ()
+;;                 (set-face-attribute
+;;                  'org-hide nil
+;;                  :foreground "#2e3735"))))
 
 ;; Export settings
+;; Use better engine for LaTeX
+(setq org-latex-pdf-process
+      (make-list 3 "xelatex -interaction nonstopmode -output-directory %o %f"))
+
 (defun my-latex-filter-headline-done (text backend info)
   "Ensure dots in headlines."
   (when (org-export-derived-backend-p backend 'latex)
@@ -79,10 +91,10 @@
        "\n\\DeclareRobustCommand{\\hsout}[1]{\\texorpdfstring{\\sout{#1}}{#1}}"))))
 
 (setq org-latex-default-packages-alist
-  '(("AUTO" "inputenc"  t)
-    ("T1"   "fontenc"   t)
-    ("" "lmodern" t)
-    (""     "fixltx2e"  nil)
+  '((""   "fontenc"   t)
+    (""     "fixltx2e"  t)
+    ("" "xltxtra" nil)
+    ("" "newunicodechar" t)
     (""     "graphicx"  t)
     (""     "longtable" nil)
     (""     "float"     nil)

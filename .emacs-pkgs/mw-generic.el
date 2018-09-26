fdc1144c2d2508b16b2e567007bb8e99f38e8ade
MetroWind <chris.corsair@gmail.com>
2018-09-26 11:52:57 -0700

    Emacs: Replace obsolete projectile settings.

--- .emacs-pkgs/mw-generic.el
+++ .emacs-pkgs/mw-generic.el
@@ -131,9 +131,10 @@
   :demand t
   :after helm-mode
   :config
-  (projectile-global-mode)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (setq projectile-completion-system 'helm)
-  (helm-projectile-on))
+  (helm-projectile-on)
+  (projectile-mode 1))
 
 (use-package twittering-mode
   :commands twit

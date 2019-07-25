3e7bb56efa1a4d2d849c50f7018aec60973faf4f
MetroWind <chris.corsair@gmail.com>
2019-07-25 11:51:08 -0700

    Emacs: Store twitter auth token

--- emacs/files/.emacs-pkgs/mw-generic.el
+++ emacs/files/.emacs-pkgs/mw-generic.el
@@ -137,7 +137,9 @@
   :config
   (setq twittering-status-format
         "%i%RT{♺}%QT{❝} @%s %FACE[font-lock-type-face]{%S} %FACE[font-lock-comment-face]{%@}\n%FOLD[    ]{%T\n%QT{+----\n%FOLD[|]{%i @%s %FACE[font-lock-type-face]{%S} %FACE[font-lock-comment-face]{%@}\n%FOLD[  ]{%T}}\n+----\n}%RT{(Retweeted from @%s)\n}")
-  (setq twittering-use-native-retweet t))
+  (setq twittering-use-native-retweet t)
+  ;; This is required to store auth token.
+  (setq twittering-use-master-password t))
 
 ;; Not through Elpa
 (load "lilypond-init.el" t)

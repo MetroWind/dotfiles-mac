1969db27be68002ad59a1b2493c0eb5fa6ffef99
MetroWind <chris.corsair@gmail.com>
2019-12-17 17:02:23 -0800

    Fix Rust test...

--- rust/test-mac.py
+++ rust/test-mac.py
@@ -8,20 +8,20 @@ import tempfile
 sys.path.append(os.path.join("..", "lib"))
 import utils
 
-class TestInstalliPython(utils.ConfigInstallTest):
+class TestRustToolchain(utils.ConfigInstallTest):
     def test_install(self):
         with tempfile.TemporaryDirectory() as TempDir:
-            Source = os.path.join(TempDir.name, "test.rs")
-            Bin = os.path.join(TempDir.name, "test")
+            Source = os.path.join(TempDir, "test.rs")
+            Bin = os.path.join(TempDir, "test")
             with open(Source, 'w') as f:
                 f.write("""fn main()
 {
     println!("aaa");
 }
 """)
-            subprocess.check_call("zsh", "--login", "-c",
-                                  "rustc -o {} {}".format(Bin, Source))
-            Output = subprocess.check_output(Bin)
+            subprocess.check_call(["zsh", "--login", "-c",
+                                   "rustc -o {} {}".format(Bin, Source)])
+            Output = subprocess.check_output([Bin,])
             self.assertEqual(Output.decode().strip(), "aaa")
 
 if __name__ == '__main__':
